a5b04d6f26b04c8350d735facf837a9906345a89
Goddid47 <mr.rexwaev@gmail.com>
2025-02-17 10:55:47 -0800

    Create __init__.py

---
+++ src/core/__init__.py
@@ -0,0 +1,6 @@
+"""
+BullX Trading Core Module
+Institutional-grade decentralized exchange trading infrastructure
+"""
+__version__ = "2.1.0"
+__all__ = ['main', 'portfolio_manager', 'solana_executor', 'ethereum_executor']
