34b57c4117a1160063a1d83eae458291f7e7523e
Goddid47 <mr.rexwaev@gmail.com>
2025-02-17 12:11:57 -0800

    Update security.py

--- src/infrastructure/security.py
+++ src/infrastructure/security.py
@@ -1,26 +1,17 @@
 from cryptography.fernet import Fernet
 from eth_account.messages import encode_defunct
-from solders.signature import Signature
+from web3 import Web3
 
 class InstitutionalSecurity:
     def __init__(self, encryption_key):
         self.cipher = Fernet(encryption_key)
-        
+    
     def encrypt_config(self, data):
         return self.cipher.encrypt(data.encode())
 
     def decrypt_config(self, encrypted_data):
         return self.cipher.decrypt(encrypted_data).decode()
 
-    def verify_transaction(self, chain, tx, signature):
-        if chain == 'ethereum':
-            return self._verify_eth_tx(tx, signature)
-        elif chain == 'solana':
-            return self._verify_solana_tx(tx, signature)
-
-    def _verify_eth_tx(self, tx, sig):
+    def verify_eth_transaction(self, tx, signature):
         message = encode_defunct(text=tx['message'])
-        return Web3().eth.account.recover_message(message, signature=sig)
-
-    def _verify_solana_tx(self, tx, sig):
-        return Signature.from_string(sig).verify(tx['public_key'])
+        return Web3().eth.account.recover_message(message, signature=signature)
